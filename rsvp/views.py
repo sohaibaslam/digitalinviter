c04e645ab55f77c13cf2db167a7a898fb1cc78d7
Sohaib Aslam <sohaib.aslam@arbisoft.com>
2020-03-11 16:48:21 +0500

    Added counts for rsvp

--- rsvp/views.py
+++ rsvp/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Case, When, Count
 
 from digitalinviter.permissions import UserLevelPermission, EventLevelPermission
 from rsvp.serializers import RSVPSerializer
@@ -24,5 +25,18 @@ class RSVPViewSet(ModelViewSet, LoginRequiredMixin):
 
     @action(detail=True)
     def get_event_rsvp(self, request, pk=None):
-        rsvp = self.get_queryset().filter(event=pk).values('user__username', 'user__profile_url', 'is_attending')
+        rsvp = self.get_queryset().filter(event=pk).values(
+            'user__username', 'user__profile_url', 'is_attending').annotate(
+            attending=Count(
+                Case(
+                    When(is_attending=True, then=1)
+                )
+            ),
+            not_attending=Count(
+                Case(
+                    When(is_attending=False, then=1)
+                )
+            )
+        )
+
         return Response(rsvp)
