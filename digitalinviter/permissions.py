58a6f87d0ec37566b0471ab47e87908fee0909b6
Sohaib Aslam <sohaib.aslam@arbisoft.com>
2020-07-05 00:53:46 +0500

    Added gallery permission

--- digitalinviter/permissions.py
+++ digitalinviter/permissions.py
@@ -12,6 +12,20 @@ class UserLevelPermission(permissions.BasePermission):
         return True
 
 
+class GalleryAccessPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method not in permissions.SAFE_METHODS:
+            if request.method == 'POST':
+                return not request.data.get('user') or request.user.id == int(request.data['user'])
+            else:
+                event_id = request.data.get('event')
+                event = event_id and Event.objects.filter(id=event_id).first()
+
+                return not event or request.user.id == event.user.id
+
+        return True
+
+
 class EventLevelPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method not in permissions.SAFE_METHODS:
