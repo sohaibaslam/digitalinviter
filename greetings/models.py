909d30af235657dc7c4ec35a52cb2c91de1a8e27
Sohaib Aslam <sohaib.aslam@arbisoft.com>
2020-07-05 01:42:26 +0500

    Corrected permission for greetings.

--- greetings/models.py
+++ greetings/models.py
@@ -8,6 +8,7 @@ class Greeting(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     message = models.TextField(max_length=1000)
+    is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user} | {self.event}'
--- greetings/views.py
+++ greetings/views.py
@@ -1,8 +1,8 @@
-from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from digitalinviter.permissions import UserLevelPermission, EventLevelPermission
 from greetings.models import Greeting
 from greetings.serializers import GreetingsSerializer
 
@@ -10,9 +10,11 @@ from greetings.serializers import GreetingsSerializer
 class GreetingsViewSet(ModelViewSet):
     serializer_class = GreetingsSerializer
     queryset = Greeting.objects.all()
-    permission_classes = [UserLevelPermission, EventLevelPermission]
 
     @action(detail=True)
     def get_event_greetings(self, request, pk=None):
         greetings = self.get_queryset().filter(event=pk).values('user__username', 'user__profile_url', 'message')
         return Response(greetings)
+
+    def get_queryset(self):
+        return Greeting.objects.filter(Q(is_approved=True) | Q(user=self.request.user))
